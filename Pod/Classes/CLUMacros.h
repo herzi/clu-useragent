0be64a274aa661b8871a88a52592828a620e380a
Sven Herzberg <sven.herzberg@cluepunk.com>
2015-06-27 17:59:28 +0200

    support Xcode versions before 6.3

---
+++ Pod/Classes/CLUMacros.h
@@ -0,0 +1,20 @@
+//
+//  CLUMacros.h
+//  CLUUserAgent
+//
+//  Created by Sven Herzberg on 27.06.15.
+//
+//
+
+/* Nullability Qualifiers were Introduced with Swift 1.2, in Xcode 6.3.
+ * Xcode 6.3 was the first version to ship with the iOS 8.3 SDK. */
+#if TARGET_OS_IPHONE
+# ifndef __IPHONE_8_3
+#  define nonnull
+#  define nullable
+#  define __nonnull
+#  define __nullable
+# endif
+#else
+# error Unsupported Platform.
+#endif
--- Pod/Classes/CLUUserAgent.h
+++ Pod/Classes/CLUUserAgent.h
@@ -7,6 +7,7 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <CLUUserAgent/CLUMacros.h>
 
 @interface CLUUserAgent : NSObject
 
