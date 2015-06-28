123855572f339a5674fd32e336640475bf7467f4
Sven Herzberg <sven.herzberg@cluepunk.com>
2015-06-28 16:26:47 +0200

    document the public API

--- CLUUserAgent/CLUUserAgent.h
+++ CLUUserAgent/CLUUserAgent.h
@@ -9,8 +9,19 @@
 #import <Foundation/Foundation.h>
 #import <CLUUserAgent/CLUMacros.h>
 
+/**
+ The class `CLUUserAgent` provides an interface to access the system's default
+ User-Agent string, as being used by `NSURLSession` and `NSURLConnection`.
+ */
 @interface CLUUserAgent : NSObject
 
+///----------------------------------------
+/// @name Mimicking the System's User-Agent
+///----------------------------------------
+
+/**
+ Returns the system's default User-Agent string.
+ */
 - (NSString*) defaultUserAgent;
 
 @end
